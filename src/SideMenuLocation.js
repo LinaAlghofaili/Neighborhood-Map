d95024c73c8c9e9f4219714ffb6813b84784264f
Linaalghofaili <lioongh@gmail.com>
2018-07-11 11:18:20 +0300

    modified src/SideMenuLocation.js

--- src/SideMenuLocation.js
+++ src/SideMenuLocation.js
@@ -7,7 +7,7 @@ export default class SideMenuLocation extends React.Component {
         return (
             <div id="container">
                 <div className="humberger-search">
-                    <input id="pac-input" className="control" type="text" placeholder="Search here..."
+                    <input id="pac-input" className="control" aria-labelledby="container pac-input" type="text" placeholder="Search here..."
                         onChange={(event) => this.props.onSearchLocation(event.target.value)} />
                 </div>
                 <LocationList selectFavourite={selectFavourite} locations={locations}/>
